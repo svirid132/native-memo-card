93ec0b4c1e1d2c2beca03510a655f5277be1c615
svirid <svirid132@gmail.com>
2021-09-29 15:34:41 +0300

    first commit

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const Card = () => {
+    return (
+      <Text>123</Text>
+    );
+}
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+          <Stack.Screen name="Home" component={Card} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+const Stack = createNativeStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
